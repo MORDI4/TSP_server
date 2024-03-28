a4245ee0b63fb687f4f531229ef687c5536d9af2
Michał <116754598+MORDI4@users.noreply.github.com>
2024-03-28 20:56:06 +0100

    Add files via upload

---
+++ tsp_server/src/main/java/com/example/tsp_server/TspServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.tsp_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TspServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TspServerApplication.class, args);
+	}
+
+}
---
+++ tsp_server/src/test/java/com/example/tsp_server/TspServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tsp_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TspServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
