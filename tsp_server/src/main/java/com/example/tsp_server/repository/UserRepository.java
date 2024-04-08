b64e651b061bfde7e235037a4cce4d1c292d2f09
MORDI4 <mmordarski4@gmail.com>
2024-04-08 17:22:48 +0200

    Files changes

--- tsp_server/src/main/java/com/example/tsp_server/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.tsp_server.repository;
-
-import com.example.tsp_server.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
