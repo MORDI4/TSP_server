6e4fdf45aaaf411005acab0de19477cf2d871ae4
jakub <jakubporeba8@gmail.com>
2024-04-15 21:40:56 +0200

    Login #1

--- src/main/java/com/example/tsp_server/model/User.java
+++ src/main/java/com/example/tsp_server/model/User.java
@@ -11,9 +11,51 @@ import jakarta.persistence.Id;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer user_id;
+    private Integer user_Id;
     private String name;
     private String password;
-    private Integer language_id;
+    private Integer languageId;
     private String login;
+
+    // Getters
+    public Integer getUserId() {
+        return user_Id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Integer getLanguageId() {
+        return languageId;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    // Setters
+    public void setUserId(Integer userId) {
+        this.user_Id = userId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setLanguageId(Integer languageId) {
+        this.languageId = languageId;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
 }
---
+++ src/main/java/com/example/tsp_server/service/UserService.java
@@ -0,0 +1,20 @@
+package com.example.tsp_server.service;
+
+import com.example.tsp_server.model.User;
+import com.example.tsp_server.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Optional<User> authenticate(String login, String password) {
+        return userRepository.findByLogin(login)
+                .filter(user -> user.getPassword().equals(password)); // prosty sposób, należy użyć lepszego mechanizmu hashowania
+    }
+}
