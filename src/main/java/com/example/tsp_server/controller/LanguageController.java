f017a1813ced591fb93f7228cf5b0410fdd46830
dominik228820 <dominik228820@gmail.com>
2024-04-28 14:27:09 +0200

    Sprint 4 Przygotowanie do zmiany języka w ustawieniach

--- src/main/java/com/example/tsp_server/controller/LanguageController.java
+++ src/main/java/com/example/tsp_server/controller/LanguageController.java
@@ -3,7 +3,7 @@ package com.example.tsp_server.controller;
 import com.example.tsp_server.service.LanguageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.http.ResponseEntity;
 import java.util.List;
 
 @RestController
@@ -20,5 +20,15 @@ public class LanguageController {
     @GetMapping("/names")
     public List<String> getAllLanguageNames() {
         return languageService.findAllLanguageNames();
+    }
+
+    @PutMapping("/users/{userId}/language")
+    public ResponseEntity<?> updateLanguage(@PathVariable Integer userId, @RequestBody Integer languageId) {
+        boolean isUpdated = languageService.updateUserLanguage(userId, languageId);
+        if(isUpdated) {
+            return ResponseEntity.ok().build(); // Zaktualizowano pomyślnie
+        } else {
+            return ResponseEntity.badRequest().body("Nie udało się zaktualizować języka lub język jest taki sam.");
+        }
     }
 }
--- src/main/java/com/example/tsp_server/service/LanguageService.java
+++ src/main/java/com/example/tsp_server/service/LanguageService.java
@@ -1,36 +1,55 @@
 package com.example.tsp_server.service;
 
 import com.example.tsp_server.model.Language;
+import com.example.tsp_server.model.User;
 import com.example.tsp_server.repository.LanguageRepository;
+import com.example.tsp_server.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.Optional;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class LanguageService {
 
     private final LanguageRepository languageRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public LanguageService(LanguageRepository languageRepository) {
+    public LanguageService(LanguageRepository languageRepository, UserRepository userRepository) {
         this.languageRepository = languageRepository;
+        this.userRepository = userRepository;
     }
 
     public List<String> findAllLanguageNames() {
-        // Pobranie wszystkich języków i zwrócenie listy ich nazw
         return languageRepository.findAll().stream()
                 .map(Language::getName)
                 .collect(Collectors.toList());
     }
 
     public Language findByName(String name) {
-        // Wyszukanie języka po nazwie, zwrócenie języka lub rzucenie wyjątku, jeśli nie znaleziono
         return languageRepository.findByName(name)
-                .orElseThrow(() -> new RuntimeException("Język nie został odnaleziony :("));
+                .orElseThrow(() -> new RuntimeException("Język nie został odnaleziony."));
     }
+
     public Optional<Language> findById(Long id) {
         return languageRepository.findById(id);
+    }
+
+    @Transactional
+    public boolean updateUserLanguage(Integer userId, Integer languageId) {
+        Optional<User> userOptional = userRepository.findById(userId);
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            if (!user.getLanguageId().equals(languageId)) {
+                user.setLanguageId(languageId);
+                userRepository.save(user);
+                return true;
+            }
+        }
+        return false;
     }
 }
